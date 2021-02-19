39529d587d4571cd97f059da00d200f7bb322559
viveksood <viveksood257@gmail.com>
2021-02-19 01:20:11 -0500

    final code without comments and no code for output file

--- project1.py
+++ project1.py
@@ -3,105 +3,126 @@ import time
 import copy
 
 
+class PriorityQueue:
+    def __init__(self,testCase):
+        self.queue = [testCase]
+        self.priorityList = [1]
 
-
+    def extentOfArrangement(self,testCase):
+        priority = 0
+        for index,element in enumerate(testCase):
+            if((index + 1) == element):
+                priority += 1
+        return priority
+    
+    def insert(self,testCase):
+        priority = self.extentOfArrangement(testCase)
+        self.queue.append(testCase)
+        self.priorityList.append(priority)
+    
+    def extract(self):
+        index = self.priorityList.index(max(self.priorityList))
+        self.priorityList.pop(index)
+        return self.queue.pop(index)
 
 
 class Tile:
 
-    def __init__(self,testCase):
+    def __init__(self,testCase,visited,size):
+        self.queue = PriorityQueue(testCase)
+        self.visited = visited
+        self.result = np.append(np.array([i for i in range(1,16)]),0)
+        self.size = np.sqrt(size)
         
-        self.queue = [testCase]
-        self.visitedDict = {}
-        
-        
-    def checkAndGenerateSignature(self,testCase):
-        signature = ""
-        finalOutput = "01050913020610140307111504081200"
-        for tile in testCase.flatten():
-            tile = str(tile)
-            if (len(tile) == 1):
-                signature += "0" + tile
-            else:
-                signature += tile
-        if(signature == finalOutput):
-            flag = True
-        else:
-            flag = False
-        return signature,flag
 
-    def mover(self,x,y,testCase,signList):
-        moverTestCase = copy.deepcopy(testCase)
-        moverTestCase[x + signList[0]][y + signList[1]], moverTestCase[x + signList[2]][y + signList[3]] = moverTestCase[x + signList[2]][y + signList[3]], 0
-        signature,flag = self.checkAndGenerateSignature(testCaseMoveTop)
-        if(flag):
-            return False
-        elif(signature not in self.visitedDict):
-            self.visitedDict[signature] = 0
-            self.queue.append(testCaseMoveTop)
-        return True
-    
-
-    def moveTile(self):
-        testCase = self.queue.pop(0)
-        x1, y1 = np.where(testCase == 0)
+    def tileMover(self):
+        queue = self.queue
+        testCase = queue.extract()
         
-        x = x1[0]
-        y = y1[0]
-        matrixSize = len(testCase)
-        if(y != 0):
-            flag = self.mover(x,y,testCase,[0,0,0,-1])
+        
+        visited = self.visited
+
+        index = np.where(testCase == 0)[0]
+        column,row = divmod(index,4)
+
+        indexMinusFour = index - 4
+        indexPlusFour  = index + 4
+        indexMinusOne  = index - 1
+        indexPlusOne   = index + 1
+
+        if(column != 0):
+            left = copy.deepcopy(testCase)
+            left[indexMinusFour], left[index] = 0, left[indexMinusFour]
+
+            if(np.array_equal(self.result,left)):
+                return True
+
+            forVisited = tuple(left)
+            if(forVisited not in visited):
+                visited[forVisited] = 0
+                queue.insert(left)
+
+        if(column != self.size - 1):
+            right = copy.deepcopy(testCase)
+            right[indexPlusFour], right[index] = 0, right[indexPlusFour]
+
+            if(np.array_equal(self.result,right)):
+                return True
+
+            forVisited = tuple(right)
+            if(forVisited not in visited):
+                visited[forVisited] = 0
+                queue.insert(right)
+                
+        if(row != 0):
+            top = copy.deepcopy(testCase)
+            top[indexMinusOne], top[index] = 0, top[indexMinusOne]
+
+            if(np.array_equal(self.result,top)):
+                return True
             
+            forVisited = tuple(top)
+            if(forVisited not in visited):
+                visited[forVisited] = 0
+                queue.insert(top)
+
+        if(row != self.size - 1):
+            down = copy.deepcopy(testCase)
+            down[indexPlusOne], down[index] = 0, down[indexPlusOne]
+
+            if(np.array_equal(self.result,down)):
+                return True
         
-        if(y != matrixSize-1):
-            testCaseMoveDown = copy.deepcopy(testCase)
-            testCaseMoveDown[x][y] = testCaseMoveDown[x][y+1]
-            testCaseMoveDown[x][y+1] = 0
-            signature,flag = self.checkAndGenerateSignature(testCaseMoveDown)
-            if(flag):
-                return False
-            elif(signature not in self.visitedDict):
-                self.visitedDict[signature] = 0
-                self.queue.append(testCaseMoveDown)
-        if(x != 0):
-            testCaseMoveLeft = copy.deepcopy(testCase)
-            testCaseMoveLeft[x][y] = testCaseMoveLeft[x-1][y]
-            testCaseMoveLeft[x-1][y] = 0
-            signature,flag = self.checkAndGenerateSignature(testCaseMoveLeft)
-            if(flag):
-                return False
-            elif(signature not in self.visitedDict):
-                self.visitedDict[signature] = 0
-                self.queue.append(testCaseMoveLeft)
-        if(x != matrixSize-1):
-            testCaseMoveRight = copy.deepcopy(testCase)
-            testCaseMoveRight[x][y] = testCaseMoveRight[x+1][y]
-            testCaseMoveRight[x+1][y] = 0
-            signature,flag = self.checkAndGenerateSignature(testCaseMoveRight)
-            if(flag):
-                return False
-            elif(signature not in self.visitedDict):
-                self.visitedDict[signature] = 0
-                self.queue.append(testCaseMoveRight)
+            forVisited = tuple(down)
+            if(forVisited not in visited):
+                visited[forVisited] = 0
+                queue.insert(down)
         
-        return True
+        return False
+        #print(left,right,top,down)
+        
+
+
+
 
 def main():
-    #testCase1 = np.array([[1, 2, 3, 4],[ 5, 6, 0, 8], [9, 10, 7, 12] , [13, 14, 11, 15]])
-    testCase1 = np.array([[1,5,9,13],[2,6,10,14],[3,7,11,15],[4,8,0,12]])
-    tile = Tile(testCase1)
-    #print(Tile.findBlankTile())
-    #tile.checkAndGenerateSignature()
-    flag = True
-    while(flag):
-        flag = tile.moveTile()
+    testCase1 = np.array([[1, 2, 3, 4],[5, 6, 0, 8],[9, 10, 7, 12],[13, 14, 11, 15]])
+    testCase2 = np.array([[1, 0, 3, 4],[ 5, 2, 7, 8], [9, 6, 10, 11] , [13, 14, 15, 12]])
+    testCase3 = np.array([[0, 2, 3, 4],[ 1,5, 7, 8], [9, 6, 11, 12] , [13, 10, 14, 15]])
+    testCase4 = np.array([[5, 1, 2, 3],[0,6, 7, 4], [9, 10, 11, 8] , [13, 14, 15, 12]])
+    testCase5 = np.array([[1, 6, 2, 3], [9,5, 7, 4], [0, 10, 11, 8] , [13, 14, 15, 12]])
+    testCase6 = np.array([[ 1,  5,  9, 13],[ 2,  6, 10, 14],[ 3,  0,  7, 11],[ 4,  8, 12, 15]])
+    
+    tile = Tile(testCase6.flatten(),{},16)
+    flag = False
+    steps = 0
+    while(not flag):
+        steps += 1
+        flag = tile.tileMover()
+    print(steps)  
+
 if __name__ == '__main__':
     start = time.time()
     main()
     end = time.time()
     print(end- start)
-
-
-
-
-
